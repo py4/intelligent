49fb1e87bee1193f100b401860a325dea87148af
Pooya <ibtkm2009@gmail.com>
2014-01-26 23:56:22 +0330

    some little changes

--- templates/header.php
+++ templates/header.php
@@ -32,6 +32,9 @@
               	{
               		?>
               		<li class="active">
+              			<a href="profile.php"><i class="fa fa-user"></i> پروفایل </a>
+              		</li>
+              		<li class="active">
               			<a href="logout.php"><i class="fa fa-sign-out"></i> خروج </a>
               		</li>
               		<?
