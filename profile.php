731de4ae92d6deb5b89bbe907795e00fe8e9e16a
Pooya <ibtkm2009@gmail.com>
2014-02-01 21:12:48 +0330

    change in profile dashboard

--- profile.php
+++ profile.php
@@ -15,39 +15,73 @@
 			پروفایل
 		</div>
 		<div class="profile_info">
-			<ul class="thumbnails">
-				<li class="span2">
-					<a href="#" class="thumbnail">
-						 <img src="avatar.png" alt="">
-					</a>
-					<p class="username">
-						ibtkm
-					</p>
-				</li>
-				<li>
-					<ul class="non_list profile_info_detail">
-						<li class="items">	
-							<i class="fa fa-question-circle"></i>
-									پویا مرادی 
-						</li>
-						<li class="items">
-							<i class="fa fa-envelope"></i>
-							ibtkm2009@gmail.com
-						</li>
-						<li class="items">
-							<i class="fa fa-phone-square"></i>
-							09124584887
+			<div class="row">
+				<div class="span1">
+				</div>
+				<div class="span5">
+					<ul class="thumbnails">
+						<li class="span3">
+							<a href="#" class="thumbnail">
+								 <img src="img/avatar.png" alt="">
+							</a>
+							<p class="username">
+								ibtkm
+							</p>
+							<ul class="non_list profile_info_detail">
+								<li class="items">	
+									<i class="fa fa-question-circle"></i>
+											پویا مرادی 
+								</li>
+								<li class="items">
+									<i class="fa fa-envelope"></i>
+									ibtkm2009@gmail.com
+								</li>
+								<li class="items">
+									<i class="fa fa-phone-square"></i>
+									09124584887
+								</li>
+							</ul>
 						</li>
 					</ul>
-				</li>
-				
-			</ul>
-		</div>
-		<div class="alert alert-info">
-			آزمون‌ها
-		</div>
-		<div class="alert alert-info">
-			مشاور
+				</div>
+				<div class="span6">
+					<div class="alert alert-success">
+						آزمون‌ها
+					</div>
+					<div class="row">
+						<div class="span2">
+							<ul class="non_list exams">
+								<li> <span class="label label-important">مانده</span> آزمون آیزنگ </li>
+								<li> <span class="label label-important">مانده</span> آزمون هالند </li>
+								<li> <span class="label label-success">دادید</span> آزمون فلان </li>
+								<li> <span class="label label-important">مانده</span> آزمون فیشر </li>
+								<li> <span class="label label-success">دادید</span> آزمون اکبر </li>
+							</ul>
+						</div>
+						<div class="span4 profile_progress">
+							<span class="label label-inverse"> پیشرفت: </span>
+							<div class="progress progress-success">
+								<div class="bar" style="width: 40%"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="span4">
+				<div class="alert alert-error">
+						مشاور
+					</div>
+					<ul class="thumbnails">
+						<li class="span2">
+							<a href="#" class="thumbnail">
+								 <img src="img/adviser.png" alt="">
+							</a>
+							<p class="username">
+								felani
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	<?
 	include 'templates/footer.php';
