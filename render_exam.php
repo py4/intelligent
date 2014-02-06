fa2db399d661f14e493f1df3f0725d0df2c1e7e1
Pooya <ibtkm2009@gmail.com>
2014-02-07 01:47:27 +0330

    getting questions and choices

--- render_exam.php
+++ render_exam.php
@@ -1,3 +1,26 @@
 <?php
-	
+	if(isset($_POST['answers']))
+	{
+
+	}
+	else
+	{
+		?>
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="exam">
+		<?php
+			$sql = "SELECT * FROM $exam_name";
+			$result = mysqli_query($connection,$sql) or die(mysqli_error($connection));
+			$questions = array();
+			while($row = mysqli_fetch_assoc($result))
+				$questions[] = $row['question_content'];
+			$sql = "SELECT * FROM choices WHERE exam_name = '$exam_name'";
+			$result = mysqli_query($connection,$sql) or die(mysqli_error($connection));
+			$choices = array();
+			while($row = mysqli_fetch_assoc($result))
+				for($i = 0; $i < $row['choice_count']; $i++)
+					$choices[] = $row['choice'.($i+1)];
+			print_r($choices);
+	}
+	//getting questions
+	//getting choices	
 ?>
--- submit_exam.php
+++ submit_exam.php
@@ -2,4 +2,7 @@
 session_start();
 include 'validate_exam.php';
 include "render_exam.php";
+// rendering questions
+
+
 ?>
